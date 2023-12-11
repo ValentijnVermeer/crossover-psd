85332f858894939b6b0ea395a4df2f913f26cb8d
Maziar Pedrami <awildroot@gmail.com>
2023-12-11 15:27:54 +0100

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Newsletter from "./components/Newsletter/Newsletter";
 import Ticket from '../src/components/Tickets'
 import Section3 from "./components/Section3";
 import SectionJohnDoe from "./components/SectionJohnDoe";
+import Banner from "../src/components/Banner"
 
 function App() {
   return (
     <>
       <Header />
+      <Banner/>
       <Section3 /> 
       <SectionJohnDoe />
       <Newsletter />
