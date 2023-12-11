93cb199dcf8af9905fe8e9b81abdd34521aadd1b
Valentijn <v.f.vermeer@gmail.com>
2023-12-11 12:38:51 +0100

    section3

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Section3 from './components/Section3'
 
 function App() {
 
 
   return (
     <>
+    <Section3 />
     </>
   )
 }
